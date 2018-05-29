cd9be87caa6f82b9f322fcb3d1ee567a3cc9581b
Khaja Minhajuddin <minhajuddink@gmail.com>
2018-05-29 13:14:42 +0530

    Make hex publishable

--- mix.exs
+++ mix.exs
@@ -4,6 +4,12 @@ defmodule Kiwi.MixProject do
   def project do
     [
       app: :kiwi,
+      name: "Kiwi",
+      description: description(),
+      package: package(),
+      docs: [
+        extras: ~W(README.md)
+      ],
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
@@ -22,8 +28,27 @@ defmodule Kiwi.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
+  end
+
+  defp description do
+    """
+    A persistent key value store for your elixir and erlang apps.
+    Data is backed by a postgres database.
+    """
+  end
+
+  defp package do
+    [
+      description: description(),
+      files: ~w(lib config mix.exs README.md LICENSE),
+      maintainers: ["Khaja Minhajuddin"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "http://github.com/minhajuddin/kiwi",
+        # TODO: Add blog post which introduces kiwi
+      }
     ]
   end
 end
